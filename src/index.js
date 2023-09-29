d4bb2dbaaed0bb596645899c750084ff0d17676a
MOOW4 <108760540+MOOW4@users.noreply.github.com>
2023-09-30 00:39:23 +0200

    fix what was messed up :)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Christian";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, Mom! <br></br>It's me {name}</h1>);
